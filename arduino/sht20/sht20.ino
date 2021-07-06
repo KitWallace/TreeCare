5ab58d64ecccc995a887214ecfda350784cfb5a7
Ian Mitchell <ian.mitchell@sky.uk>
2021-07-06 21:53:23 +0100

    Changed to demo from electropeak.

--- arduino/sht20/sht20.ino
+++ arduino/sht20/sht20.ino
@@ -1,22 +1,32 @@
-#include "uFire_SHT20.h"
-uFire_SHT20 sht20;
+/*
+modified on Apr 10, 2021
+Modified by MehranMaleki from Arduino Examples
+https://electropeak.com/learn/interfacing-sht20-temperature-and-humidity-sensor-with-arduino/
+*/
+
+#include <Wire.h>
+#include "DFRobot_SHT20.h"
+
+DFRobot_SHT20 sht20;
 
 void setup()
 {
-  Serial.begin(9600);
-  Wire.begin();
-  sht20.begin();
+    Serial.begin(115200);
+    Serial.println("SHT20 Example!");
+    sht20.initSHT20();                         // Init SHT20 Sensor
+    delay(100);
+    sht20.checkSHT20();                        // Check SHT20 Sensor
 }
 
 void loop()
 {
-  sht20.measure_all();
-  Serial.println((String)sht20.tempC + "°C");
-  Serial.println((String)sht20.tempF + "°F");
-  Serial.println((String)sht20.dew_pointC + "°C dew point");
-  Serial.println((String)sht20.dew_pointF + "°F dew point");
-  Serial.println((String)sht20.RH + " %RH");
-  Serial.println((String)sht20.vpd() + " kPa VPD");
-  Serial.println();
-  delay(5000);
+    float humd = sht20.readHumidity();         // Read Humidity
+    float temp = sht20.readTemperature();      // Read Temperature
+    Serial.print(" Temperature: ");
+    Serial.print(temp, 1);
+    Serial.print("C");
+    Serial.print("\t Humidity: ");
+    Serial.print(humd, 1);
+    Serial.println("%");
+    delay(1000);
 }
